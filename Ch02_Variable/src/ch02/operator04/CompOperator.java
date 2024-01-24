7bbd5fffe2ec6b885edfd1cbb7670ab96eaadbc1
Seulki <sgdream95@gmail.com>
2024-01-24 16:53:47 +0900

    Ch02 연사자 true/false

---
+++ Ch02_Variable/src/ch02/operator04/CompOperator.java
@@ -0,0 +1,21 @@
+package ch02.operator04;
+
+public class CompOperator {
+	public static void main(String[] args) {
+		int num0 = 100;
+		int num1 = 10;
+		int num2 = 100;
+		
+		boolean bVal = num0 > num1;
+		System.out.println(bVal); // true
+		
+		bVal = num0 < num1;
+		System.out.println(bVal); // false
+		
+		bVal = num0 == num2;
+		System.out.println(bVal); // true
+		
+		bVal = num0 >= num1;
+		System.out.println(bVal); // true 
+	}
+}
