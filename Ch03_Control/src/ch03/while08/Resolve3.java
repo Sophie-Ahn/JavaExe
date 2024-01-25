a45323d2aadbb75307bcb6985b200dea75e36a15
Sophie-Ahn <sgdream95@gmail.com>
2024-01-25 16:55:50 +0900

    Ch03 while문 문제풀

---
+++ Ch03_Control/src/ch03/while08/Resolve3.java
@@ -0,0 +1,13 @@
+package ch03.while08;
+
+public class Resolve3 {
+	public static void main(String[] args) {
+		int i = 0;
+		int result = 0;
+		while (i < 10) {
+			i++;
+			result += i;
+		}
+		System.out.println(result);
+	}
+}
