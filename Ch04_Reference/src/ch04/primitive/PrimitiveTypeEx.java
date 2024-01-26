f5b094d4668a1cb9cb1f0a6cc365aac7c690da5b
Sophie-Ahn <sgdream95@gmail.com>
2024-01-26 12:22:13 +0900

    Ch04 참조형 프로젝트 생

---
+++ Ch04_Reference/src/ch04/primitive/PrimitiveTypeEx.java
@@ -0,0 +1,20 @@
+package ch04.primitive;
+
+public class PrimitiveTypeEx {
+	public static void main(String[] args) {
+		byte b = 10;
+		char ch = 'A';
+		short s = 10000;
+		int i = 99999999;
+		long l = 99999999999999L;
+		float f = 3.14f;
+		double d = 3.14;
+		System.out.println(Byte.SIZE);
+		System.out.println(Character.SIZE);
+		System.out.println(Short.SIZE);
+		System.out.println(Integer.SIZE);
+		System.out.println(Long.SIZE);
+		System.out.println(Float.SIZE);
+		System.out.println(Double.SIZE);
+	}
+}
