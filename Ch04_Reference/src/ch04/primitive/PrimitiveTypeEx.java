09dad303603fb10a39381cd86174b1f22ff69c67
Sophie-Ahn <sgdream95@gmail.com>
2024-01-26 12:46:35 +0900

    Ch04 배열 소스

--- Ch04_Reference/src/ch04/primitive/PrimitiveTypeEx.java
+++ Ch04_Reference/src/ch04/primitive/PrimitiveTypeEx.java
@@ -2,6 +2,9 @@ package ch04.primitive;
 
 public class PrimitiveTypeEx {
 	public static void main(String[] args) {
+		// 아래 변수는 Stack에 생성되었다가
+		// main의 영역을 벗어나면 Stack에서 사라진다.
+		// 지역변수
 		byte b = 10;
 		char ch = 'A';
 		short s = 10000;
