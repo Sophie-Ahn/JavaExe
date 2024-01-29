ecb2f2c62798f34daab857fa126ad09fdf42b657
Sophie-Ahn <sgdream95@gmail.com>
2024-01-29 11:20:28 +0900

    Ch04 문제 변경으로 인한 답 변경

--- Ch05.Array/src/ch05/array04/Resolve04.java
+++ Ch05.Array/src/ch05/array04/Resolve04.java
@@ -2,24 +2,14 @@ package ch05.array04;
 
 public class Resolve04 {
 	public static void main(String[] args) {
-		int[] score = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int[] store = new int[3];
-		int count = 0;
+		int[] num = new int[10];
 		
-		for (int i = 0; i < score.length; i++) {
-			if (score[i] % 3 == 0) {
-				store[count] = score[i];
-				count++;
-			}
+		for (int i = 0; i < num.length; i++) {
+			num[i] = (i + 1) * 3;
 		}
 		
-		for (int i = 2; i < 3; i--) {
-			if (0 <= i) {				
-				System.out.println(store[i]);
-			}
+		for (int i = num.length - 1; i >= 0; i--) {
+			System.out.println(num[i] + "");
 		}
-		
-		
-	
 	}
 }
