2fb6d8f820d10d55ded2da4eda3ed517604e5ce2
Sophie-Ahn <sgdream95@gmail.com>
2024-01-30 11:46:07 +0900

    Ch05 배열 선생님문제풀이

--- Ch05.Array/src/ch05/array08/Resolve3.java
+++ Ch05.Array/src/ch05/array08/Resolve3.java
@@ -15,7 +15,24 @@ public class Resolve3 {
 		for (int i = 0; i < numArr.length; i++) {
 			numArr[i] = num[i];
 		}
-		System.out.println(Arrays.toString(num));
-
+		System.out.println(Arrays.toString(numArr));
+		
+		
+		// 선생님풀이
+		/*
+		 * numArr.length/2까지 하면 앞과 뒤의 값 모두 접근
+		 * 만약 numArr.length로 하면 다시 원래대로 돌아감
+		 */
+		for (int i = 0; i < numArr.length; i++) {
+			int fIdx = i;
+			int lIdx = numArr.length - 1 - i;
+			int pre = numArr[fIdx];
+			int suf = numArr[lIdx];
+			
+			numArr[fIdx] = suf;
+			numArr[lIdx] = pre;
+			
+		}
+		System.out.println(Arrays.toString(numArr));
 	}
 }
--- Ch05.Array/src/ch05/array08/Resolve4.java
+++ Ch05.Array/src/ch05/array08/Resolve4.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Resolve4 {
 	public static void main(String[] args) {
-		int[] arr = {7, 100, 20, 4, 66};
+		int[] arr = {7, 100, 20, 4, 66, 90, 99, 1000, 55, 3, 5, 101010};
 
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 1; j < arr.length; j++) {
@@ -16,5 +16,43 @@ public class Resolve4 {
 			}
 		}
 		System.out.println(Arrays.toString(arr));
+		
+		// 선택적 정렬
+		int[] numArr = new int[10000];
+		// 난수로 0~100000 사이의 임의의 정수를 배열에 저장
+		for (int i =0; i < numArr.length; i++) {
+			double dVal = Math.random();
+			numArr[i] = (int)(dVal * 100000);
+		}
+		for (int i = 0; i < numArr.length; i++) {
+			// i번째 데이터를 선택한다.
+			// i를 최소값으로 가정한다.
+			int sel = numArr[i];
+			int minIdx = i;
+			// i다음부터 마지막까지 반복
+			for (int j = i + 1; j < numArr.length; j++) {
+				if(sel > numArr[j]) {
+					//선택을 교체한다.
+					minIdx = j;
+					sel = numArr[j];
+				}
+			}
+			
+			int temp = numArr[i];
+			numArr[i] = numArr[minIdx];
+			numArr[minIdx] = temp;
+			
+		}
+		
+		// 버블정렬
+		for (int i = 0; i < arr.length - 1; i++) {
+			for(int j = 0; j < arr.length - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
 	}
 }
