9edf3dd3711690ef7745c870185dee8e795e9db8
Sophie-Ahn <sgdream95@gmail.com>
2024-02-02 17:55:51 +0900

    Ch09 문제풀이 수정

--- Ch09_Extends/src/ch09/resolve14/resolve1/PrintMange.java
+++ Ch09_Extends/src/ch09/resolve14/resolve1/PrintMange.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 public class PrintMange {
 	private Scanner sc = new Scanner(System.in);
 	
-	private int choosePrint() {
-		while(true) {
+	private void choosePrint() {
+		boolean isRun = true;
+		while(isRun) {
 			System.out.println("프린터를 선택하시오");
 			System.out.println("1. 잉크타입프린트 | 2. 레이저타입프린트");
 			int num = sc.nextInt();
-			PrintInfo printInfo = null;
 			
 			switch(num) {
-			case 1:
-//				printInfo = PrintInfo.print();
-				break;
-			case 2:
-				break;
-			default:
-				
+				case 1:
+					break;
+				case 2:
+					break;
+				default:
+					isRun = false;
 			}
 		}
 	}
