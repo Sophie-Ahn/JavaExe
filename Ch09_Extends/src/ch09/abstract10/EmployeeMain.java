c8ad7df61dc6f76a0f881f77904095f47ac2a492
Sophie-Ahn <sgdream95@gmail.com>
2024-02-02 11:14:14 +0900

    Ch09 employee 추상클래스 소스

--- Ch09_Extends/src/ch09/abstract10/EmployeeMain.java
+++ Ch09_Extends/src/ch09/abstract10/EmployeeMain.java
@@ -2,6 +2,7 @@ package ch09.abstract10;
 
 public class EmployeeMain {
 	public static void main(String[] args) {
-		// 메인 메서드
+		EmployeeManager em = new EmployeeManager();
+		em.run(); // run메서드 호출
 	}
 }
