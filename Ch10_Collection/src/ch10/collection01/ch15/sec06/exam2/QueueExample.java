e02183a67b855730fbd308939e3553a2720aefec
DESKTOP-NOMV6IF\user <pleasantckh@naver.com>
2024-02-05 22:05:31 +0900

    컬렉션 예제소스

---
+++ Ch10_Collection/src/ch10/collection01/ch15/sec06/exam2/QueueExample.java
@@ -0,0 +1,32 @@
+package ch10.collection01.ch15.sec06.exam2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        // Queue 컬렉션 생성
+        Queue<Message> messagesQueue = new LinkedList<>();
+
+        // 메시지 넣기
+        messagesQueue.offer(new Message("sendMail", "홍길동"));
+        messagesQueue.offer(new Message("sendSMS", "신용권"));
+        messagesQueue.offer(new Message("sendKakaotalk", "감자바"));
+
+        // 메시지를 하나씩 꺼내어 처리
+        while (!messagesQueue.isEmpty()) {
+            Message message = messagesQueue.poll();
+            switch (message.command) {
+                case "sendMail":
+                    System.out.println(message.to + "님에게 메일을 보냅니다.");
+                    break;
+                case "sendSMS":
+                    System.out.println(message.to + "님에게 SMS를 보냅니다.");
+                    break;
+                case "sendKakaotalk":
+                    System.out.println(message.to + "님에게 카카오톡을 보냅니다.");
+                    break;
+            }
+        }
+    }
+}
