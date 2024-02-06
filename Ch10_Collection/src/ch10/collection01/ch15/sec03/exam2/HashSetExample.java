b8675ab93eb07a487609b38ebbde0bdc06081c46
Sophie-Ahn <sgdream95@gmail.com>
2024-02-06 15:51:18 +0900

    Ch10 set hashcode 소스 추가

--- Ch10_Collection/src/ch10/collection01/ch15/sec03/exam2/HashSetExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec03/exam2/HashSetExample.java
@@ -7,10 +7,27 @@ public class HashSetExample {
 	public static void main(String[] args) {
 		// HashSet 컬렉션 생성
 		Set<Member> set = new HashSet<Member>();
+	
+		// Member 객체 저장
+//		set.add(new Member("홍길동", 30));
+//		set.add(new Member("홍길동", 30)); // 인스턴스는 다르지만 동등한 객체이므로 1개만 저장됨
+
+		// 위의 두줄을 풀어쓴 것이 아래것
+		Member mem0 = new Member("홍길동", 30);
+		Member mem1 = new Member("홍길동", 30);
+		
+		System.out.println(mem0.hashCode());
+		System.out.println(mem1.hashCode());
 		
 		// Member 객체 저장
-		set.add(new Member("홍길동", 30));
-		set.add(new Member("홍길동", 30)); // 인스턴스는 다르지만 동등한 객체이므로 1개만 저장됨
+		set.add(mem0);
+		set.add(mem1); // 내부에서 hashCode() -> equals() 비교/걸러짐
+		
+		
+		// 저장된 객체 수 출력
+		System.out.println("총 객체 수: " + set.size());
+		
+		set.add(new Member("임꺽정", 30));
 		
 		// 저장된 객체 수 출력
 		System.out.println("총 객체 수: " + set.size());
--- Ch10_Collection/src/ch10/collection01/ch15/sec03/exam3/HashSetExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec03/exam3/HashSetExample.java
@@ -2,9 +2,19 @@ package ch10.collection01.ch15.sec03.exam3;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class HashSetExample {
+	public static void printAll(Iterator<String> iterator) {
+		while(iterator.hasNext()) {
+			// 객체를 하나 가져오기
+			String element = iterator.next();
+			System.out.println(element);
+		}
+	}
+	
 	public static void main(String[] args) {
 		// HashSet 컬렉션 생성
 		Set<String> set = new HashSet<String>();
@@ -15,7 +25,26 @@ public class HashSetExample {
 		set.add("JSP");
 		set.add("Spring");
 		
+		List<String> list = new LinkedList<String>();
+		
+		// 객체 추가
+		set.add("홍길동");
+		set.add("임꺽정");
+		set.add("장길산");
+		set.add("일지매");
+		set.add("차돌바위");
+		set.add("머털이");
+		
+		/*
+		 * 다른 컬렉션들도 Iterator인터페이스 상속을 받아 구현
+		 * Iterator는 데이터의 처믕부터 계속 끝까지 가져오는데 사용한다.
+		 * 알고리즘이 다른 컬렉션을 Iterator라는 1가지 방식으로 가져 올 수
+		 * 있게 하였으므로 효율성을 가져옴.
+		 * */
+		
 		// 객체를 하나씩 가져와서 처리
+//		printAll(set.iterator());
+//		printAll(list.iterator());
 		Iterator<String> iterator = set.iterator();
 		while(iterator.hasNext()) {
 			// 객체를 하나 가져오기
