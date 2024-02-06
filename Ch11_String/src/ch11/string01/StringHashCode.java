04a12a74838ec6ddafbe98c9a3b883f73eb1259d
Sophie-Ahn <sgdream95@gmail.com>
2024-02-06 17:35:16 +0900

    Ch10 collection linkedlist 예시

---
+++ Ch11_String/src/ch11/string01/StringHashCode.java
@@ -0,0 +1,20 @@
+package ch11.string01;
+
+public class StringHashCode {
+	public static void main(String[] args) {
+		String str0 = "Hello, Java";
+		String str1 = "Hello, Java";
+		String str2 = new String("Hello, Java");
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		/*
+		 * String의 hashCode()는 일반 객체의 hashCode와 다르다
+		 * 일반객체는 주소를 hash화 하여 제공하지만
+		 * 그러므로 객체 공간이 다른 여부와 관계없이
+		 * 문자열 
+		 */
+	}
+}
