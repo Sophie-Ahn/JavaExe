def9c6329da6216ef45ed29ef8abb2a2c92c9f09
Sophie-Ahn <sgdream95@gmail.com>
2024-02-08 14:27:26 +0900

    Ch14 줄바꿈

--- Ch14_Exception/src/ch14/exception01/ch11/sec06/AccountExample.java
+++ Ch14_Exception/src/ch14/exception01/ch11/sec06/AccountExample.java
@@ -3,6 +3,7 @@ package ch14.exception01.ch11.sec06;
 public class AccountExample {
 	public static void main(String[] args) {
 		Account account = new Account();
+		
 		// 예금하기
 		account.deposit(10000);
 		System.out.println("예금액: " + account.getBalance());
